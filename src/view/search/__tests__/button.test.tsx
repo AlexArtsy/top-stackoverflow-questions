8000acf70d36bcc57255a201397c208d1ebd4318
AlexArts <a_arz@mail.ru>
2026-07-09 00:37:52 +0400

    Мелкая правка

--- src/view/search/__tests__/button.test.tsx
+++ src/view/search/__tests__/button.test.tsx
@@ -1,7 +1,7 @@
 /** @vitest-environment jsdom */
 
 import { describe, expect, it } from 'vitest';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import questionsReducer from '../../../store/question-slice';
